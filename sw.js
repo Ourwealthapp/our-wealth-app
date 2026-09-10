23b40d26ce9c94e5bfba11b4550656306f40f525
Ourwealthapp <boldinglamontae@gmail.com>
2026-09-09 23:44:01 -0400

    Refactor asset caching in service worker

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
 const CACHE_NAME = 'ourwealth-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
